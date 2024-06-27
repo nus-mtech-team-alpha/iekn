aa52b3fa0c11f282506b0b7062d92e4f1f2fa6cc
wq <e1112885@u.nus.edu>
2024-06-28 01:19:33 +0800

    session history api

--- cookbook/llms/ollama/auto_rag/main.py
+++ cookbook/llms/ollama/auto_rag/main.py
@@ -17,22 +17,22 @@ def get_root():
 
 @app.get("/sessions")
 def get_sessions():
-    # return {"sessions":"ses"}
     auto_rag_assistant: Assistant = get_auto_rag_assistant()
     sessions = []
+
     if auto_rag_assistant.storage:
         rag_runs: List[AssistantRun] = auto_rag_assistant.storage.get_all_runs()
 
         for r in rag_runs:
             sessions.append({"run_id":r.run_id,"run_name":r.run_name})
-    #         # st.sidebar.button(str(r.run_id if r.run_name is None else r.run_name), r.run_id, type="primary" if r.run_id == st.session_state["auto_rag_assistant_run_id"] else "secondary", on_click=load_history, args=[r.run_id], use_container_width=True)
     return JSONResponse(sessions)
 
 @app.get("/sessions/{run_id}/history")
 def get_sessions_history(run_id):
     auto_rag_assistant = get_auto_rag_assistant(run_id=run_id)
-    print(len(auto_rag_assistant.storage.get_all_runs()))
+    auto_rag_assistant.read_from_storage()
+
     assistant_chat_history = auto_rag_assistant.memory.get_chat_history()
     print(auto_rag_assistant.run_id)
-    print(assistant_chat_history)
+    print("hist len:",len(assistant_chat_history))
     return JSONResponse(assistant_chat_history) 
